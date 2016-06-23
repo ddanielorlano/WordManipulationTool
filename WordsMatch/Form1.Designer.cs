6969c246f06de6cd543cb9814f14b68db3174d30
Daniel Orlan <ddanielorlano@gmail.com>
2016-06-23 11:45:58 -0400

    anchor to tab control

--- WordsMatch/Form1.Designer.cs
+++ WordsMatch/Form1.Designer.cs
@@ -186,6 +186,9 @@
             // 
             // tabControl1
             // 
+            this.tabControl1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
             this.tabControl1.Controls.Add(this.matcherPage);
             this.tabControl1.Controls.Add(this.formatterTab);
             this.tabControl1.Location = new System.Drawing.Point(-2, 3);
